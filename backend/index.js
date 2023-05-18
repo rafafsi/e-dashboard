64b340bc8c79dc7c446fd9808e2655f758ccfe00
Rafaela Inácio <rafaelafsinacio@gmail.com>
2023-05-18 06:53:34 -0300

    models and db connection

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cors = require("cors");
+require("./db/config");
+
+const User = require("./db/User");
+const Product = require("./db/Product");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.post("/register", async (req, res) => {
+  let user = new User(req.body);
+  let result = await user.save();
+  result = result.toObject();
+  delete result.password;
+  res.send(result);
+});
+
+app.post("/login", async (req, res) => {
+  if (req.body.password && req.body.email) {
+    let user = await User.findOne(req.body).select("-password");
+    if (user) {
+      res.send(user);
+    } else {
+      res.send({ result: "No user was found!" });
+    }
+  } else {
+    res.send({ result: "No user was found!" });
+  }
+});
+
+app.listen(5000);
